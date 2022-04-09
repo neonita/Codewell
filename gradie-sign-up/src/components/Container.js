ecb67f32ba01ef18234c6d728884dad0de3d3d51
neonita <neonita888@gmail.com>
2022-04-09 01:13:08 -0400

    Structure and style iphone mockup section/container and form section/container

--- gradie-sign-up/src/components/Container.js
+++ gradie-sign-up/src/components/Container.js
@@ -1,8 +1,22 @@
+import Mockup from "../assets/iPhone-Mockup.png";
+
 const Container = () => {
   return (
-    <section className="md:min-w-[900px] md:min-h-[600px] lg:rounded-lg bg-red-100">
-      hi from Container
-    </section>
+    <main className="md:w-[900px] md:h-[600px] lg:rounded-lg border border-slate-400 overflow-hidden flex">
+      {/* left */}
+      <section className="w-1/2 h-full bg-theme-blue p-14 text-center">
+        <h1 className="text-4xl text-white font-medium">Gradie</h1>
+        <p className="text-theme-light-blue text-xl">
+          Beautiful gradients in seconds.
+        </p>
+        <img src={Mockup} className="relative top-14" alt="iPhone mockup" />
+      </section>
+
+      {/* right */}
+      <section className="w-1/2 h-full bg-white p-14">
+        <h1 className="text-4xl text-theme-black font-medium">Login</h1>
+      </section>
+    </main>
   );
 };
 
--- gradie-sign-up/tailwind.config.js
+++ gradie-sign-up/tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
-    color: {
-      "theme-blue": "#013243",
-      "theme-light-blue": "#7eb6ca",
-      "theme-black": "#010100",
-    },
     fontFamily: {
-      roboto: ["Roboto, Arial, sans-serif"],
+      sans: ["Roboto, Arial, sans-serif"],
     },
     extend: {
       backgroundImage: {
         "bg-gradient": "url('./assets/Background.png')",
+      },
+      colors: {
+        "theme-blue": "#013243",
+        "theme-light-blue": "#7eb6ca",
+        "theme-black": "#010100",
       },
     },
   },
