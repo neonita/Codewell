1a00ac27de61a41e11e594fbc2030f9a8352d4f3
neonita <neonita888@gmail.com>
2022-04-09 17:37:35 -0400

    Create Form component

--- gradie-sign-up/src/components/Container.js
+++ gradie-sign-up/src/components/Container.js
@@ -1,10 +1,11 @@
 import Mockup from "../assets/iPhone-Mockup.png";
+import Form from "./Form";
 
 const Container = () => {
   return (
-    <main className="md:w-[900px] md:h-[600px] lg:rounded-lg border border-slate-400 overflow-hidden flex">
+    <main className="md:w-[900px] md:h-[600px]  flex">
       {/* left */}
-      <section className="w-1/2 h-full bg-theme-blue p-14 text-center">
+      <section className="w-1/2 h-full bg-theme-blue p-14 text-center lg:rounded-tl-xl lg:rounded-bl-xl">
         <h1 className="text-4xl text-white font-medium">Gradie</h1>
         <p className="text-theme-light-blue text-xl">
           Beautiful gradients in seconds.
@@ -13,9 +14,10 @@ const Container = () => {
       </section>
 
       {/* right */}
-      <section className="w-1/2 h-full bg-white p-14">
+      {/* <section className="w-1/2 h-full bg-white p-14 border border-slate-400 lg:rounded-tr-xl lg:rounded-br-xl">
         <h1 className="text-4xl text-theme-black font-medium">Login</h1>
-      </section>
+      </section> */}
+      <Form />
     </main>
   );
 };
---
+++ gradie-sign-up/src/components/Form.js
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <section className="w-1/2 h-full bg-white p-14 border border-slate-400 lg:rounded-tr-xl lg:rounded-br-xl">
+      <h1 className="text-4xl text-theme-black font-medium">Login</h1>
+    </section>
+  );
+};
+
+export default Form;
