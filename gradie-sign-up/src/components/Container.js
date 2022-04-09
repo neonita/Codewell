c29c41d5f32ad0166f45413942f7708abc95075b
neonita <neonita888@gmail.com>
2022-04-09 17:49:23 -0400

    Resize iphone mockup image

--- gradie-sign-up/src/components/Container.js
+++ gradie-sign-up/src/components/Container.js
@@ -5,18 +5,19 @@ const Container = () => {
   return (
     <main className="md:w-[900px] md:h-[600px]  flex">
       {/* left */}
-      <section className="w-1/2 h-full bg-theme-blue p-14 text-center lg:rounded-tl-xl lg:rounded-bl-xl">
+      <section className="w-1/2 h-full bg-theme-blue pt-14 px-14 pb-0 text-center lg:rounded-tl-xl lg:rounded-bl-xl relative">
         <h1 className="text-4xl text-white font-medium">Gradie</h1>
         <p className="text-theme-light-blue text-xl">
           Beautiful gradients in seconds.
         </p>
-        <img src={Mockup} className="relative top-14" alt="iPhone mockup" />
+        <img
+          src={Mockup}
+          className="absolute bottom-0 left-0 w-full h-auto"
+          alt="iPhone mockup"
+        />
       </section>
 
       {/* right */}
-      {/* <section className="w-1/2 h-full bg-white p-14 border border-slate-400 lg:rounded-tr-xl lg:rounded-br-xl">
-        <h1 className="text-4xl text-theme-black font-medium">Login</h1>
-      </section> */}
       <Form />
     </main>
   );
