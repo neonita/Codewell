3aed524962a2b06875436fdec06ec92e548fcd70
neonita <neonita888@gmail.com>
2022-04-08 23:37:57 -0400

    Set initial setup of project, import theme font: Roboto

---
+++ gradie-sign-up/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto, Arial, sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
